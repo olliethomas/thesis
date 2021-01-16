eabc3394bbc48841c07f5c688812a05832f531a4
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-01-16 14:34:51 +0000

    make a cronjob

---
+++ count.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+dir=$1
+ext=$2
+totalNumberOfWords=0
+
+for file in $(find $dir -name "*.$ext"); do
+  wordCount=$(textutil -convert txt -stdout $file | wc -w)
+  totalNumberOfWords=$((totalNumberOfWords + wordCount))
+done  
+
+today=$(date +%Y-%m-%d-%H-%M)
+
+echo "${today}, ${totalNumberOfWords}" >> ~/Development/thesis/content/thesis_wc.csv
